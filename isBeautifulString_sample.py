c831ded8ff6bf2d8de827d9d9bbc896360ced5a1
cdjoe <43168295+cdjoe@users.noreply.github.com>
2023-12-20 15:10:12 +0800

    Update isBeautifulString_sample.py

--- isBeautifulString_sample.py
+++ isBeautifulString_sample.py
@@ -1,4 +1,4 @@
-#// A string is said to be beautiful if each letter in the string appears at most as many times as the previous letter in the alphabet within the string;
+#A string is said to be beautiful if each letter in the string appears at most as many times as the previous letter in the alphabet within the string;
 #**ie:** *b* occurs no more times than *a*; *c* occurs no more times than *b*; etc. Note that letter *a* has no previous letter.
 ###### Example:
 #For inputString = "*bbbaacdafe*", the output should be **solution(inputString) = true**.
